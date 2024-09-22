b780f5862952ed4c331b11fb2182343f91d7f33d
Altuğ Altuner <vefgend@gmail.com>
2024-09-22 15:50:22 +0300

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from 'next-pwa';
+
+const nextConfig = withPWA({
+    dest: 'public',  // Servis çalışanının kaydedileceği yer
+    disable: process.env.NODE_ENV === 'development', // Geliştirme modunda PWA'yı devre dışı bırak
+});
 
 export default nextConfig;
