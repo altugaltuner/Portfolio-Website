09f7fe3d4d99ae72cc2dc74a2a6afcd1224b34c8
Altuğ Altuner <vefgend@gmail.com>
2024-10-01 12:44:51 +0300

    m

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,52 +1,33 @@
-import { openai } from '@ai-sdk/openai';
-import { streamText, convertToCoreMessages, StreamData } from 'ai';
-import { NextResponse } from 'next/server';
-import nodemailer from 'nodemailer';
+// import { NextResponse } from 'next/server';
+// import nodemailer from 'nodemailer';
 
-// Allow streaming responses up to 30 seconds
-// export const maxDuration = 30;
+// export async function POST(req: Request) {
+//     const { firstname, lastname, email, phone, message } = await req.json();
 
-export async function POST(req: Request) {
+//     const transporter = nodemailer.createTransport({
+//         service: 'Gmail',
+//         auth: {
+//             user: process.env.GMAIL_USER,
+//             pass: process.env.GMAIL_PASS,
+//         },
+//     });
 
-    const { messages } = await req.json();
-    const data = new StreamData();
+//     const mailOptions = {
+//         from: email,
+//         to: 'altugaltuner6@gmail.com',
+//         subject: `New message from: ${firstname} ${lastname}`,
+//         text: `
+//             Name: ${firstname} ${lastname}
+//             Email: ${email}
+//             Phone: ${phone}
+//             Message: ${message}
+//         `,
+//     };
 
-    const result = await streamText({
-        model: openai('gpt-4-turbo'),
-        messages: convertToCoreMessages(messages),
-        onFinish() {
-            data.close();
-        },
-    });
-    return result.toDataStreamResponse({ data });
-}
-
-//burası mail gönderme kısmı
-// const { firstname, lastname, email, phone, message } = await req.json();
-
-// const transporter = nodemailer.createTransport({
-//     service: 'Gmail',
-//     auth: {
-//         user: process.env.GMAIL_USER,
-//         pass: process.env.GMAIL_PASS,
-//     },
-// });
-
-// const mailOptions = {
-//     from: email,
-//     to: 'altugaltuner6@gmail.com',
-//     subject: `Yeni mesaj: ${firstname} ${lastname}`,
-//     text: `
-//   İsim: ${firstname} ${lastname}
-//   E-posta: ${email}
-//   Telefon: ${phone}
-//   Mesaj: ${message}
-// `,
-// };
-
-// try {
-//     await transporter.sendMail(mailOptions);
-//     return NextResponse.json({ message: 'Email sent successfully!' }, { status: 200 });
-// } catch (error) {
-//     return NextResponse.json({ message: 'Error sending email', error }, { status: 500 });
+//     try {
+//         await transporter.sendMail(mailOptions);
+//         return NextResponse.json({ message: 'Email sent successfully!' }, { status: 200 });
+//     } catch (error) {
+//         return NextResponse.json({ message: 'Error sending email', error }, { status: 500 });
+//     }
 // }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -28,38 +28,6 @@ const info = [
 
 const Contact = () => {
 
-    const [formData, setFormData] = useState({
-        firstname: '',
-        lastname: '',
-        email: '',
-        phone: '',
-        message: ''
-    });
-
-    const handleSubmit = async (e: { preventDefault: () => void }) => {
-        e.preventDefault();
-
-        try {
-            const res = await fetch('/api/contact', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(formData),
-            });
-
-            if (!res.ok) {
-                throw new Error('Form submission failed');
-            }
-
-            const result = await res.json();
-            alert('Email successfully sent!');
-        } catch (error) {
-            console.error('Error submitting form:', error);
-            alert('Error sending email. Please try again.');
-        }
-    };
-
     return (
         <motion.section
             initial={{ opacity: 0 }}
@@ -69,45 +37,50 @@ const Contact = () => {
             <div className="container mx-auto">
                 <div className="flex flex-col xl:flex-row gap-[30px]">
                     <div className="xl:h-[54%] order-2 xl:order-none">
-                        <form onSubmit={handleSubmit} className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+                        <form action="https://formsubmit.co/vefgend@gmail.com" method="POST" className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
                             <h3 className="text-4xl text-accent">Let's work together</h3>
                             <p className="text-white/60">
                                 You can contact me by filling this form</p>
                             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+
                                 <Input
-                                    type="text"
-                                    placeholder="firstname"
-                                    value={formData.firstname}
-                                    onChange={(e) => setFormData({ ...formData, firstname: e.target.value })}
-                                />
-                                <Input
-                                    type="text"
-                                    placeholder="lastname"
-                                    value={formData.lastname}
-                                    onChange={(e) => setFormData({ ...formData, lastname: e.target.value })}
+                                    type="hidden"
+                                    placeholder="_subject"
+                                    value="new message from your website"
                                 />
                                 <Input
                                     type="email"
+                                    name="email"
                                     placeholder="Email"
-                                    value={formData.email}
-                                    onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                                    required
                                 />
                                 <Input
                                     type="tel"
+                                    name="phone"
                                     placeholder="phone number"
-                                    value={formData.phone}
-                                    onChange={(e) => setFormData({ ...formData, phone: e.target.value })}
+                                    required
                                 />
                             </div>
                             <Textarea
                                 className="h-[200px]"
+                                name="message"
                                 placeholder="type your message here"
-                                value={formData.message}
-                                onChange={(e) => setFormData({ ...formData, message: e.target.value })}
+                                required
                             />
                             <Button size="md" className="max-w-40" type="submit">Send Message</Button>
                         </form>
                     </div>
+
+
+
+
+
+
+
+
+
+
+
 
                     <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
                         <ul className="flex flex-col gap-10">
