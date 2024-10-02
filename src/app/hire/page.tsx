2515a04920ff6f27e5e201aae8409a515de97bfc
Altuğ Altuner <vefgend@gmail.com>
2024-10-02 15:22:13 +0300

    m

--- src/app/hire/page.tsx
+++ src/app/hire/page.tsx
@@ -1,15 +1,29 @@
 "use client";
 
-import Chatbot from "@/components/AiChat";
+import { useChat } from "ai/react";
 
-const Home = () => {
+export default function Chat() {
+    const { messages, input, handleInputChange, handleSubmit } = useChat();
 
     return (
-        <div className="p-14">
-            <Chatbot />
-
-        </div>
+        <div className="mx-auto w-full max-w-md py-24 flex flex-col">
+            {messages.length > 0 ?
+                messages.map((m) => (
+                    <div key={m.id} className="whitespace-pre-wrap ">
+                        {m.role === "user" ? "You: " : "AI: "}
+                        {m.content}
+                    </div>
+                )) : null
+            }
+            < form onSubmit={handleSubmit} className="flex flex-row">
+            </form>
+            <input
+                type="text"
+                value={input}
+                placeholder="Type your message here"
+                onChange={handleInputChange}
+                className="fixed w-full max-w-md bottom-0 border border-gray-300"
+            />
+        </div >
     );
-};
-
-export default Home;
+}
